ce81db1cdc44634f7af754064a2563d48e4656d4
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-09-22 19:37:41 -1000

    planets links show up...

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let darthyRequest = new XMLHttpRequest();
 darthyRequest.addEventListener("load", function(){
   let darthVader = JSON.parse(this.responseText);
   document.getElementById("person4Name").innerHTML = darthVader.name;
+  console.log(darthVader.homeworld, "DARTH HOME");
 
   let planet = new XMLHttpRequest();
   planet.addEventListener("load", function(){
@@ -25,11 +26,13 @@ hanRequest.addEventListener("load", function(){
   document.getElementById("person14Name").innerHTML = hanSolo.name;
 
   let species = new XMLHttpRequest();
-species.addEventListener("load", function(){
-  let species = JSON.parse(this.responseText);
-  document.getElementById("person14Species").innerHTML = species.name;
+  species.addEventListener("load", function(){
+    let species = JSON.parse(this.responseText);
+    document.getElementById("person14Species").innerHTML = species.name;
   });
+
   species.open("GET", hanSolo.species);
+  console.log(hanSolo.species);
   species.send();
 });
 hanRequest.open("GET", "http://swapi.co/api/people/14/");
@@ -37,27 +40,42 @@ hanRequest.send();
 
 
 
-function createList(input){
-  var x = document.createElement("LI");
-  var t = document.createTextNode(input);
-  x.appendChild(t);
-  document.getElementById("filmList").appendChild(x);
-}
 
 
 
 let filmsRequest = new XMLHttpRequest();
 filmsRequest.addEventListener("load", function(){
-  let sWFilms = JSON.parse(this.responseText).results;
-
-  sWFilms.map(function(film){
-    createList(film.title);
+  let films = JSON.parse(this.responseText).results;
+  films.map(function(movie){
+    createListFilm(movie.title);// <-- attaching to HTML
   });
+  let planetsRequest = new XMLHttpRequest();
 
- console.log(sWFilms.planets);
+  let planets = JSON.parse(this.responseText);
+  // THIS GIVES ME EACH PLANETS LINK
+  films[0].planets.forEach(function(planetLink){
+    createListPlanet(planetLink);
+    console.log(planetLink);
+     });
+    planetsRequest.open("GET", films[0].planets[0]);
+    planetsRequest.send();
 
 });
 filmsRequest.open("GET", "http://swapi.co/api/films/");
 filmsRequest.send();
 
 
+
+function createListFilm(input){
+  var x = document.createElement("LI");
+  var t = document.createTextNode(input);
+  x.appendChild(t);
+  document.getElementById("filmList").appendChild(x);
+}
+
+function createListPlanet(input){
+  var x = document.createElement("LI");
+  var t = document.createTextNode(input);
+  x.appendChild(t);
+  document.getElementById("planetList").appendChild(x);
+}
